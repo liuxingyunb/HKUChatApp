b12d4b41d27288551bb75ad09b09abda1f4a0a2e
Jdream1109 <276748475@qq.com>
2023-04-08 17:38:43 +0800

    初步实现个人聊天功能及用户的注册与登录

--- src/test/java/com/example/chatapp/ChatAppApplicationTests.java
+++ src/test/java/com/example/chatapp/ChatAppApplicationTests.java
@@ -1,33 +1,48 @@
 package com.example.chatapp;
 
 import com.example.chatapp.dao.UserDao;
+import com.example.chatapp.model.po.Personal_chat;
 import com.example.chatapp.model.po.User;
+import com.example.chatapp.service.Personal_chatService;
 import com.example.chatapp.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChatAppApplicationTests {
 
     @Autowired(required = false)
     UserDao userDao;
     @Autowired
     UserService userService;
+    @Autowired
+    Personal_chatService personal_chatService;
 
     @Test
     void contextLoads() {
     }
 
-    @Test
-    void UserTest() {
-        //User user = new User("test1","12313312");
-
-        //userService.addUser(user);
-        //userService.getUserByUsername("test1");
-        userService.deleteUser(7);
-        User user=userService.getUserByUsername("J");
-        System.out.println(user);
-    }
+//    @Test
+//    void UserTest() {
+//        //User user = new User("test1","12313312");
+//
+//        //userService.addUser(user);
+//        //userService.getUserByUsername("test1");
+//        userService.deleteUser(7);
+//        User user=userService.getUserByUsername("J");
+//        System.out.println(user);
+//    }
+//    @Test
+//    void personChat(){
+//        Date date=new Date();
+//        Personal_chat personalChat=new Personal_chat(1,2,"text","test123",date);
+//        personal_chatService.addPersonal_chat(personalChat);
+//    }
 
 }
