96e7936fae0c61cf17c95f7e98ea34a20a86d60a
Jiacheng-Gao <2889886695@qq.com>
2023-04-10 14:48:40 +0800

    聊天初步功能

---
+++ src/main/java/com/example/chatapp/service/ChatService.java
@@ -0,0 +1,6 @@
+package com.example.chatapp.service;
+
+public interface ChatService {
+
+
+}
---
+++ src/main/java/com/example/chatapp/service/impl/ChatServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.chatapp.service.impl;
+
+import com.example.chatapp.service.ChatService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatServiceImpl implements ChatService {
+
+}
